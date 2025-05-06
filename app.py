ca7f56c6b2a73f7bb9c3cbf3c3f1869e4123edf8
nyakoOG <inakioterog@gmail.com>
2025-05-06 03:10:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -50,5 +50,9 @@ def place_order():
     except BinanceAPIException as e:
         return jsonify({'error': str(e)}), 400
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
+
